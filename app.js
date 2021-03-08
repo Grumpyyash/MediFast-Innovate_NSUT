7b71ef7921877ad6ff1492d950ad73579beda9b3
Yash Vardhan <yashvardhan513@gmail.com>
2021-03-08 16:24:44 +0530

    added procfile

--- app.js
+++ app.js
@@ -294,7 +294,11 @@ app.get("/maps",(req,res)=>{
 });
 
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server has started successfully");
 });
